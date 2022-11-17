decf9fc4c472a04a6665a45b2487b7f0c9f6ba01
pxe05 <79581464+pxe05@users.noreply.github.com>
2022-11-17 17:32:02 +0100

    test(it01h06): Create acceptance test (valid case)

---
+++ backEnd/src/main/java/com/ei10391048/project/modelo/GeoCodingService.java
@@ -0,0 +1,11 @@
+package com.ei10391048.project.modelo;
+
+public class GeoCodingService {
+    private static GeoCodingService instance;
+
+    private GeoCodingService(){}
+
+    public static GeoCodingService getInstance(){
+        return null;
+    }
+}
---
+++ backEnd/src/main/java/com/ei10391048/project/modelo/Location.java
@@ -0,0 +1,8 @@
+package com.ei10391048.project.modelo;
+
+public class Location {
+    private String name;
+    public Location(String name) {
+        this.name = name;
+    }
+}
