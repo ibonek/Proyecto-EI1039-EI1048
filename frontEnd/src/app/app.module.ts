ea81e6a5b10877fcf27567fa5c3e306dd0e0531f
al394756 <al394756@uji.es>
2022-11-25 19:39:13 +0100

    feat(it01h06): Created prototype top-bar and theme

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
