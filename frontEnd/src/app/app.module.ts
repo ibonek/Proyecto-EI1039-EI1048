a994d59a75690afdf9e7ecda84ba7a1c3a54b9bb
ibonek <al394759@uji.es>
2022-11-29 18:34:36 +0100

    feat(it01h06): Created from and input

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -6,18 +6,32 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatListModule} from "@angular/material/list";
+import { LocationFromComponent } from './location-from/location-from.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatOptionModule} from "@angular/material/core";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    LocationFromComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatOptionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontEnd/src/app/location-from/location-from.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {startWith, map} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-location-from',
+  templateUrl: './location-from.component.html',
+  styleUrls: ['./location-from.component.css']
+})
+export class LocationFromComponent implements OnInit {
+  control = new FormControl();
+  locations: string[] = ['Londres', 'Valencia', 'Madrid', 'Paris'];
+  filteredLocations: Observable<string[]> | undefined;
+
+  ngOnInit() {
+    this.filteredLocations = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = this._normalizeValue(value);
+    return this.locations.filter(location => this._normalizeValue(location).includes(filterValue));
+  }
+
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
+  }
+}
