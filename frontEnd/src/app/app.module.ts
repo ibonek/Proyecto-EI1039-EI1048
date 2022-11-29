3fa4511831cc901ae65555b802943a1dfd0b7917
ibonek <al394759@uji.es>
2022-11-29 16:39:36 +0100

    feat(it01h06): Created top-bar logo button prototype

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
