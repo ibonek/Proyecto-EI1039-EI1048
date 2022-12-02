3be6eb8cf54cb7f1761c64423761518994f581f4
David del Río <78787392+al394756@users.noreply.github.com>
2022-12-02 19:10:03 +0100

    test(it01h06): Testing History 6 completed!

---
+++ backEnd/src/test/java/com/ei10391048/project/GeoCodServiceTest.java
@@ -0,0 +1,47 @@
+package com.ei10391048.project;
+
+import com.ei10391048.project.exception.IncorrectLocationException;
+import com.ei10391048.project.modelo.ByName;
+import com.ei10391048.project.modelo.GeoCodService;
+import com.ei10391048.project.modelo.Location;
+import com.ei10391048.project.modelo.LocationManager;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class GeoCodServiceTest {
+    /**
+     * Test que comprueba la historia de usuario 6: Como usuario quiero dar de alta una ubicación a partir de un
+     * topónimo, con el fin de tenerla disponible en el sistema.
+     *
+     * @throws IncorrectLocationException
+     */
+    @Test
+    public void addLocationByNameValid() throws IncorrectLocationException {
+        GeoCodService geoCodSrv = new GeoCodService();
+        String name = "Valencia";
+        geoCodSrv.setSearch(new ByName(name));
+
+        LocationManager locations = LocationManager.getInstance();
+        int num = locations.getNumberOfLocations();
+
+        locations.setLocationApi(geoCodSrv);
+        locations.addByName();
+
+        assertEquals( locations.getNumberOfLocations(), num + 1);
+    }
+
+
+    @Test
+    public void addLocationByNameInvalid() throws IncorrectLocationException {
+        try {
+            String toponimo = "";
+            GeoCodService geoCodSrv = new GeoCodService();
+            geoCodSrv.setSearch(new ByName(toponimo));
+            Location localizacion = geoCodSrv.findLocation();
+        } catch (IncorrectLocationException ex){
+            assertTrue(true);
+        }
+}
+}
