ff9d31965553fb2efb3460555b91c7ff9db52b4a
al394756 <al394756@uji.es>
2022-12-03 02:41:59 +0100

    test(it01-h09): Created formatingInputCoordsTest and transformCoordsTest to test if the coordinates are ok even if the user introduces geographical coordinates (º, ', ")

--- backEnd/src/test/java/com/ei10391048/project/controlador/FindingLocationControllerTest.java
+++ backEnd/src/test/java/com/ei10391048/project/controlador/FindingLocationControllerTest.java
@@ -1,5 +1,6 @@
 package com.ei10391048.project.controlador;
 
+import org.junit.Assert;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -27,6 +28,41 @@ class FindingLocationControllerTest {
     }
 
     @ParameterizedTest
+    @MethodSource("coordsDataFormat")
+    public void formatingInputCoordsTest(String input, double[] sol) {
+        Assert.assertArrayEquals(FindingLocationController.formatingInputCoords(input), sol,0.1);
+    }
+
+    static Stream<Arguments> coordsDataFormat() {
+        return Stream.of(
+                Arguments.of("100.1 50.3", new double[]{100.1, 50.3}),
+                Arguments.of("100.1, 50.3", new double[]{100.1, 50.3}),
+                Arguments.of("100.1 50.3", new double[]{100.1, 50.3}),
+                Arguments.of("36º59'32\"N 6º26'37\"W", new double[]{36.9921, -6.4435}),
+                Arguments.of("36º59'32\"S, 6º26'37\"E", new double[]{-36.9921, 6.4435})
+                );
+    }
+
+    @ParameterizedTest
+    @MethodSource("transformCoordsData")
+    public void transformCoordsTest(String coord, double value){
+        assertEquals(FindingLocationController.transformCoords(coord),value,0.1);
+    }
+    static Stream<Arguments> transformCoordsData() {
+        return Stream.of(
+                Arguments.of("36º59'32\"N",36.9921),
+                Arguments.of("36º59'32\"S", -36.9921),
+                Arguments.of("6º26'37\"E",  6.4435),
+                Arguments.of("6º26'37\"O", -6.4435),
+                Arguments.of("6º26'37\"W", -6.4435)
+
+
+
+
+                );
+    }
+
+    @ParameterizedTest
     @MethodSource("NameOrCoordData")
     public void isCoordinatesTest(String input, boolean sol){
         assertEquals(FindingLocationController.isCoordinates(input),sol);
@@ -37,6 +73,8 @@ class FindingLocationControllerTest {
                 Arguments.of("madrid", false),
                 Arguments.of("MaDRid", false),
                 Arguments.of("  MADRID", false),
+                Arguments.of("Castellón de la Plana", false),
+                Arguments.of("Washington DC", false),
                 Arguments.of("100.05, 50.02", true),
                 Arguments.of(" 100.41, 64.01", true),
                 Arguments.of("100º10'12\", 10º11'15\"", true),
@@ -46,9 +84,6 @@ class FindingLocationControllerTest {
                 Arguments.of("100.05N, 10.11W",true),
                 Arguments.of("100.05N, 10.11E",true),
                 Arguments.of("100.05S, 10.11O",true)
-
-
-
                 );
     }
 
