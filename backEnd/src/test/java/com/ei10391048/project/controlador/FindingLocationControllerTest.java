b922e819eb5309653f9f36af3c2ea7c54528fafe
al394756 <al394756@uji.es>
2022-12-03 01:39:28 +0100

    test(it01-h09): Created isCoordinatesTest to know if a string is a coordinate or a name

--- backEnd/src/test/java/com/ei10391048/project/controlador/FindingLocationControllerTest.java
+++ backEnd/src/test/java/com/ei10391048/project/controlador/FindingLocationControllerTest.java
@@ -12,20 +12,45 @@ class FindingLocationControllerTest {
 
 
     @ParameterizedTest
-    @MethodSource("dataFormat")
-    public void formatingInputTest(String input, String sol) {
-        assertEquals(FindingLocationController.formatingInput(input), sol);
+    @MethodSource("nameDataFormat")
+    public void formatingInputNameTest(String input, String sol) {
+        assertEquals(FindingLocationController.formatingInputName(input), sol);
     }
 
-    static Stream<Arguments> dataFormat() {
+    static Stream<Arguments> nameDataFormat() {
         return Stream.of(
                 Arguments.of("madrid", "Madrid"),
                 Arguments.of("MaDRid", "Madrid"),
                 Arguments.of("MADRID", "Madrid"),
-                Arguments.of(" Madrid", "Madrid"),
-                Arguments.of("Madrid ", "Madrid"),
                 Arguments.of("Pekín", "Pekin")
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("NameOrCoordData")
+    public void isCoordinatesTest(String input, boolean sol){
+        assertEquals(FindingLocationController.isCoordinates(input),sol);
+    }
+
+    static Stream<Arguments> NameOrCoordData() {
+        return Stream.of(
+                Arguments.of("madrid", false),
+                Arguments.of("MaDRid", false),
+                Arguments.of("  MADRID", false),
+                Arguments.of("100.05, 50.02", true),
+                Arguments.of(" 100.41, 64.01", true),
+                Arguments.of("100º10'12\", 10º11'15\"", true),
+                Arguments.of("100.05 50.02",true),
+                Arguments.of("100º10'12\" 10º11'15\"", true),
+                Arguments.of("100.05N, 10.11O",true),
+                Arguments.of("100.05N, 10.11W",true),
+                Arguments.of("100.05N, 10.11E",true),
+                Arguments.of("100.05S, 10.11O",true)
+
+
+
+                );
+    }
+
 }
 
