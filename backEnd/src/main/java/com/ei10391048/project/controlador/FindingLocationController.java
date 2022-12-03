46aa82ac1af600c8a7cab8090a6b68665956690e
al394756 <al394756@uji.es>
2022-12-03 01:45:30 +0100

    feat(it01-h09): Created isCoordinates to know if a string is a coordinate or a name

--- backEnd/src/main/java/com/ei10391048/project/controlador/FindingLocationController.java
+++ backEnd/src/main/java/com/ei10391048/project/controlador/FindingLocationController.java
@@ -6,6 +6,8 @@ import com.ei10391048.project.modelo.LocationManager;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class FindingLocationController {
@@ -23,26 +25,59 @@ public class FindingLocationController {
     @PostMapping("/addLocation")
     public void createLocation(@RequestBody String location) {
 
-        String locationName = formatingInput(location);
-        //Comprobamos que sea coordenada o name
+        location=location.trim();
+        if (isCoordinates(location)){
 
-        //Llamamos a quien sea necesario
-        LocationManager locationManager = LocationManager.getInstance();
+        }else{
+            location = formatingInputName(location);
+            //Comprobamos que sea coordenada o name
 
-        locationManager.setLocationApi(new GeoCodService());
-        locationManager.getLocationApi().setSearch(new ByName(locationName));
 
-        try {
-            locationManager.addByName();
-            confirmation=true;
-        } catch (IncorrectLocationException ex){
-            confirmation=false;
+            //Llamamos a quien sea necesario
+            LocationManager locationManager = LocationManager.getInstance();
+
+            locationManager.setLocationApi(new GeoCodService());
+            locationManager.getLocationApi().setSearch(new ByName(location));
+
+            try {
+                locationManager.addByName();
+                confirmation=true;
+            } catch (IncorrectLocationException ex){
+                confirmation=false;
+            }
         }
-
-
-
     }
 
+
+    public static boolean isCoordinates(String input){
+
+        String[] array = input.split(",");
+        if (array.length == 2 ){
+            return checkLatLonCorrect(array);
+        }
+        array = input.split(" ");
+        if (array.length == 2 ){
+            return checkLatLonCorrect(array);
+        }
+        return false;
+    }
+
+    private static boolean checkLatLonCorrect(String[] array){
+        if (array[0].contains("º") && array[0].contains("'") && array[0].contains("\"") && array[1].contains("º") && array[1].contains("'") && array[1].contains("\"")){
+            return true;
+        }
+        if ((array[0].toUpperCase().contains("N") || array[0].toUpperCase().contains("S")) && (array[1].toUpperCase().contains("O")) ||array[1].toUpperCase().contains("W") ||array[1].toUpperCase().contains("E")){
+            return true;
+        }
+        try{
+            Double.parseDouble(array[0]);
+            Double.parseDouble(array[1]);
+
+            return true;
+        } catch (Exception ex){
+            return false;
+        }
+    }
 
     @GetMapping("/addLocation")
     public Boolean giveConfirmation(){
@@ -52,11 +87,7 @@ public class FindingLocationController {
 
     }
 
-    public static String formatingInput(String input){
-        //Quitamos espacios
-        input=input.trim();
-
-
+    public static String formatingInputName(String input){
         //Quitamos tildes
         input=StringUtils.stripAccents(input);
         //Ponemos inicial en mayúscula
@@ -64,10 +95,14 @@ public class FindingLocationController {
         return input;
     }
 
+
     public static void main(String[] args) {
-        String a=" 2 1";
-        double b = Double.parseDouble(a);
-        System.out.println(b);
+        String a="2º 1".strip();
+        String[] c = a.split(",");
+        //a.substring(a.)
+
+        //double b = Double.parseDouble(c[0]);
+        System.out.println(Arrays.toString(Arrays.stream(c).toArray()));
     }
 
 }
