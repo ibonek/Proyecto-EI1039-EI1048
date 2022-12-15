6e3f8e0310e6845ccc0ecb36be126ea06da7455f
David del Río <78787392+al394756@users.noreply.github.com>
2022-12-15 18:46:58 +0100

    test(it02h18):Created integration test with Mocks for ByName and ByCoordinates

--- backEnd/src/test/java/com/ei10391048/project/modelo/ByNameTest.java
+++ backEnd/src/test/java/com/ei10391048/project/modelo/ByNameTest.java
@@ -5,6 +5,7 @@ import com.github.prominence.openweathermap.api.OpenWeatherMapClient;
 import com.github.prominence.openweathermap.api.enums.Language;
 import com.github.prominence.openweathermap.api.enums.UnitSystem;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,30 +15,53 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
-/*
-class ByNameTest {
-    private OpenWeatherMapClient mock;
-    private String apiKey = "4d8fb5b93d4af21d66a2948710284366";
 
-    @ParameterizedTest
-    @MethodSource("cities")
-    public void testAPIFindingLocationByName(String city) throws IncorrectLocationException {
-        ByName mock = Mockito.mock(ByName.class);
-        when(mock.search().getName()).thenReturn(city);
-        OpenWeatherMapClient openWeatherMapClient = new OpenWeatherMapClient(apiKey);
-        String result = openWeatherMapClient.currentWeather().single().byCityName(city).language(Language.SPANISH).unitSystem(UnitSystem.METRIC).retrieve().asJava().getLocation().getName();
-        assertEquals(city, result);
-        Mockito.verify(mock).search().getCoordinates();
+public class ByNameTest {
+    private ByName byNameMock;
+
+    @BeforeEach
+    public void setUp(){
+        byNameMock= Mockito.mock(ByName.class);
+    }
+    @Test
+    public void registerLocation_invalidName() throws IncorrectLocationException {
+        when(byNameMock.search()).thenThrow(new IncorrectLocationException());
+        LocationManager manager = LocationManager.getInstance();
+        int num = manager.getNumberOfLocations();
+
+        GeoCodService geoCodService = new GeoCodService();
+        geoCodService.setSearch(byNameMock);
+        manager.setLocationApi(geoCodService);
+        try {
+            manager.addLocation();
+            fail();
+        } catch (IncorrectLocationException ex){
+            assertTrue(true);
+        }finally {
+            assertEquals(num,manager.getNumberOfLocations());
+
+        }
+
     }
 
-    static Stream<Arguments> cities() {
-        return Stream.of(
-                Arguments.of("Madrid"),
-                Arguments.of("Valencia"),
-                Arguments.of("Torreblanca"),
-                Arguments.of("Melilla")
-        );
+    @Test
+    public void registerLocation_validName() throws IncorrectLocationException {
+        Location place = new Location();
+        place.setName("Valencia");
+        when(byNameMock.search()).thenReturn(place);
+
+        LocationManager manager = LocationManager.getInstance();
+        int num = manager.getNumberOfLocations();
+
+        GeoCodService geoCodService = new GeoCodService();
+        geoCodService.setSearch(byNameMock);
+        manager.setLocationApi(geoCodService);
+        manager.addLocation();
+        assertEquals(num+1,manager.getNumberOfLocations());
+        assertEquals(manager.getLocations().get(num), place);
     }
+
+
+
 }
 
- */
