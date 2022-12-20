af406b0fed3c4b49cc1576b340f06124c9371241
pxe05 <79581464+pxe05@users.noreply.github.com>
2022-12-20 13:24:44 +0100

    refactor(it02h23.3): Improved and finalized tests. Added getLocation test

--- backEnd/src/test/java/com/ei10391048/project/fireBase/FireBaseTest.java
+++ backEnd/src/test/java/com/ei10391048/project/fireBase/FireBaseTest.java
@@ -2,11 +2,19 @@ package com.ei10391048.project.fireBase;
 
 import com.ei10391048.project.exception.NotSavedException;
 import com.ei10391048.project.modelo.Location;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.testng.Assert.assertNull;
 import static org.testng.Assert.assertTrue;
+import java.util.stream.Stream;
 
 public class FireBaseTest {
     private static CRUDFireBase crudFireBase;
@@ -34,5 +42,35 @@ public class FireBaseTest {
         } catch (NotSavedException exception){
             assertTrue(true);
         }
+    }
+
+    @Test
+    public void getLocationFromBBDDValid(){
+        try {
+            crudFireBase.getLocation(location);
+            assertTrue(true);
+        } catch (ExecutionException | InterruptedException exception){
+            fail();
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("locations")
+    public void getLocationFromBBDDInvalid(Location location) throws ExecutionException, InterruptedException {
+        assertNull(crudFireBase.getLocation(location));
+    }
+
+    static Stream<Arguments> locations(){
+        return Stream.of(
+                Arguments.of(new Location("Teruel", 40.345, -0.6687)),
+                Arguments.of(new Location("Valencia", 39.4697600, -0.3773900)),
+                Arguments.of(new Location("Madridd", 40.4167754, -3.7037902)),
+                Arguments.of((Object) null)
+        );
+    }
+
+    @AfterAll
+    public static void tearDown() throws ExecutionException, InterruptedException {
+        crudFireBase.deleteLocations();
     }
 }
