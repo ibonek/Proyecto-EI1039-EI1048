a9dceb02984b8490b03a415133695572962e8e0e
ibonek <al394759@uji.es>
2022-12-22 23:09:57 +0100

    refactor(it02h12): moved event list to a new component

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {LocationFormComponent} from "./location-form/location-form.component";
 import {LocationListComponent} from "./location-list/location-list.component";
+import {LocationEventsComponent} from "./location-events/location-events.component";
 
 
 const routes: Routes= [
 
   {path: 'addLocation', component: LocationFormComponent},
-  {path: 'locationList', component: LocationListComponent}
+  {path: 'locationList', component: LocationListComponent},
+  {path: '', component: LocationEventsComponent}
 
   ];
 
