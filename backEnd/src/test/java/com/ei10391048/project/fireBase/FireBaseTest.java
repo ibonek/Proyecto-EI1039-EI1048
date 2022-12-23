56dc4ab4a7065d9ff099d8c33635432890cf3493
pxe05 <79581464+pxe05@users.noreply.github.com>
2022-12-23 02:31:49 +0100

    bug(it02h23.1): created acceptance tests

--- backEnd/src/test/java/com/ei10391048/project/fireBase/FireBaseTest.java
+++ backEnd/src/test/java/com/ei10391048/project/fireBase/FireBaseTest.java
@@ -1,13 +1,16 @@
 package com.ei10391048.project.fireBase;
 
+import com.ei10391048.project.exception.AlreadyActiveLocation;
 import com.ei10391048.project.exception.NotSavedException;
 import com.ei10391048.project.modelo.Location;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.testng.Assert;
 
 import java.util.concurrent.ExecutionException;
 
@@ -20,8 +23,8 @@ import java.util.stream.Stream;
 public class FireBaseTest {
     private static CRUDFireBase crudFireBase;
     private static Location location;
-    @BeforeAll
-    public static void setUp() throws ExecutionException, InterruptedException {
+    @BeforeEach
+    public void setUp() throws ExecutionException, InterruptedException {
         location = new Location("Teruel", 40.345, -0.6667);
         crudFireBase = new CRUDFireBase();
         crudFireBase.deleteLocations();
@@ -51,9 +54,7 @@ public class FireBaseTest {
         try {
             crudFireBase.addLocation(location);
             Location location2 = crudFireBase.getLocation(location);
-            assertTrue(location2.equals(location));
-        } catch (ExecutionException | InterruptedException exception){
-            fail();
+            Assert.assertEquals(location, location2);
         } catch (NotSavedException e) {
             throw new RuntimeException(e);
         }
@@ -74,6 +75,81 @@ public class FireBaseTest {
         );
     }
 
+    @Test
+    public void deleteLocationsFromBBDDValid(){
+        try {
+            crudFireBase.addLocation(location);
+            crudFireBase.deleteLocations();
+            assertEquals(0, crudFireBase.getLocations().size());
+        } catch (ExecutionException | InterruptedException exception){
+            fail();
+        } catch (NotSavedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void deleteLocationsFromBBDDInvalid(){
+        try {
+            crudFireBase.deleteLocations();
+            assertEquals(0, crudFireBase.getLocations().size());
+        } catch (ExecutionException | InterruptedException exception){
+            fail();
+        }
+    }
+
+    @Test
+    public void getLocationsFromBBDDValid(){
+        try {
+            crudFireBase.addLocation(location);
+            assertEquals(1, crudFireBase.getLocations().size());
+        } catch (NotSavedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void getLocationsFromBBDDInvalid(){
+        assertEquals(0, crudFireBase.getLocations().size());
+    }
+
+    @Test
+    public void activateLocationFromBBDDValid(){
+        try {
+            location.setActive(false);
+            crudFireBase.addLocation(location);
+            crudFireBase.activateLocation(location);
+            assertTrue(crudFireBase.getLocation(location).getIsActive());
+        } catch (NotSavedException | AlreadyActiveLocation exception) {
+            fail();
+        }
+    }
+
+    @Test
+    public void activateLocationFromBBDDInvalid() {
+        location=null;
+        try {
+            crudFireBase.activateLocation(location);
+            fail();
+        } catch (NotSavedException e) {
+            assertTrue(true);
+        } catch (AlreadyActiveLocation e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void activateLocationFromBBDDInvalid2() {
+        location.setActive(true);
+        try {
+            crudFireBase.activateLocation(location);
+            fail();
+        } catch (NotSavedException e) {
+            fail();
+        } catch (AlreadyActiveLocation e) {
+            assertTrue(true);
+        }
+    }
     @AfterAll
     public static void tearDown() throws ExecutionException, InterruptedException {
         crudFireBase.deleteLocations();
