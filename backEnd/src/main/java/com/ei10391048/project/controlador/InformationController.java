076c6828eb4ea2cad23c3b4ac84e1f22212bec2f
al394756 <al394756@uji.es>
2022-12-25 17:15:23 +0100

    feat(it03h22): Created back-end methods to communicate with front-end

--- backEnd/src/main/java/com/ei10391048/project/controlador/InformationController.java
+++ backEnd/src/main/java/com/ei10391048/project/controlador/InformationController.java
@@ -1,13 +1,13 @@
 package com.ei10391048.project.controlador;
 
+import com.ei10391048.project.exception.IncorrectLocationException;
+import com.ei10391048.project.exception.NonExistingAPIException;
 import com.ei10391048.project.modelo.*;
 import com.ei10391048.project.modelo.ApiFacade;
 import com.ei10391048.project.modelo.api.API;
 import com.ei10391048.project.modelo.api.APIsNames;
 import com.ei10391048.project.modelo.information.APIInformation;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -16,6 +16,7 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
 public class InformationController {
 
+    private Boolean confirmation =  null;
     @GetMapping("/apiInfo")
     public  List<List<List<APIInformation>>> getAPIsInfo() {
         LocationManager locationManager = LocationManager.getInstance();
@@ -37,5 +38,21 @@ public class InformationController {
 
     }
 
+    @PostMapping("/changeActiveApiState")
+    public void changeActiveApiState(@RequestBody int order) {
+        LocationManager manager = LocationManager.getInstance();
+        try {
+            manager.changeApiState(order);
+            confirmation = true;
+        } catch (NonExistingAPIException e) {
+            confirmation = false;
+        }
+    }
 
+    @GetMapping("/changeActiveApiState")
+    public Boolean giveConfirmation(){
+        while (confirmation==null);
+
+        return confirmation;
+    }
 }
