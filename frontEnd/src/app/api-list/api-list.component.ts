3c7c53691ae5ce76d67d9207e2c1d9b5566336fa
al394756 <al394756@uji.es>
2022-12-25 17:33:31 +0100

    feat(it03h22): Front-end communication with back-end complete

--- frontEnd/src/app/api-list/api-list.component.ts
+++ frontEnd/src/app/api-list/api-list.component.ts
@@ -3,6 +3,7 @@ import {Location} from "../location";
 import {ActivatedRoute, Router} from "@angular/router";
 import {FindingByNameService} from "../finding-by-name.service";
 import {Api} from "../api";
+import Swal from "sweetalert2";
 
 
 
@@ -26,7 +27,58 @@ export class ApiListComponent implements OnInit {
     );
   }
 
-  changeActiveState(api: Api | undefined) {
-    this.findingByNameService.changeActiveStateApi(api).subscribe(data=> {});
+  changeActiveState(order : number) {
+    this.findingByNameService.changeActiveStateApi(order).subscribe(data=> {
+      this.findingByNameService.giveAPIConfirmation().subscribe(confirmation =>{
+        if (confirmation){
+          this.tinySuccessAlert(this.apis[order].isActive, this.apis[order].name)
+        }else{
+          this.tinyFailAlert();
+        }
+      })
+    });
+  }
+
+  async tinySuccessAlert(active: boolean, name: string) {
+    if (active){
+      Swal.fire({
+        title: 'Ok!',
+        text: 'You will see information of '+name+' about your locations.',
+        icon: 'success',
+        showCancelButton: false,
+        confirmButtonColor: '#c2185b',
+        confirmButtonText: 'Ok'
+      }).then(( result => {
+        if (result.isConfirmed) {
+          window.location.reload()
+        }
+      }))
+    } else{
+      Swal.fire({
+        title: 'Ok!',
+        text: 'You will no longer see information of '+name+' about your locations.',
+        icon: 'success',
+        showCancelButton: false,
+        confirmButtonColor: '#c2185b',
+        confirmButtonText: 'Ok'
+      }).then(( result => {
+        if (result.isConfirmed) {
+          window.location.reload()
+        }
+      }))
+    }
+
+
+  }
+
+  async tinyFailAlert(){
+    await Swal.fire({
+      title: 'Ooops',
+      text: 'There is an error and you can\'t change that api state',
+      icon: 'error',
+      confirmButtonColor: '#c2185b',
+      confirmButtonText: 'Ok'
+    })
+
   }
 }
